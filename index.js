b7473b63069420bece10eaa9663c20339ff351b7
Franco Giuntoli <franco.giuntoli@gmail.com>
2021-04-13 17:49:37 +0200

    IT4 & 5 done

--- index.js
+++ index.js
@@ -19,9 +19,21 @@ class SortedList {
  
   }
 
-  max() {}
+  max() {
+    if(this.items.length == 0){
+      throw new Error('EmptySortedList');
+    }else{
+      return Math.max(...this.items);
+    }
+  }
 
-  min() {}
+  min() {
+    if(this.items.length == 0){
+      throw new Error('EmptySortedList');
+    }else{
+      return Math.min(...this.items);
+    }
+  }
 
   sum() {}
 
